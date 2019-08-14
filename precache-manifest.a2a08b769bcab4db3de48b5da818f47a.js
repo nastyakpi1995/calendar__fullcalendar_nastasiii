b547ccf71f15032f87425edde9cad8ca4dc8b02a
Nastia Krat <nastyakpi1995@gmail.com>
2019-08-14 23:55:58 +0300

    Updates

---
+++ precache-manifest.a2a08b769bcab4db3de48b5da818f47a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7e0b639f3937927a02daa8d524393d",
+    "url": "/react_phone-catalog/index.html"
+  },
+  {
+    "revision": "c0485a70f44e1ae0438b",
+    "url": "/react_phone-catalog/static/css/main.1a16b686.chunk.css"
+  },
+  {
+    "revision": "ff5c07e3c92851db1982",
+    "url": "/react_phone-catalog/static/js/2.285cbe29.chunk.js"
+  },
+  {
+    "revision": "c0485a70f44e1ae0438b",
+    "url": "/react_phone-catalog/static/js/main.b27496ef.chunk.js"
+  },
+  {
+    "revision": "9b3e1d68b9cc1dd9a925",
+    "url": "/react_phone-catalog/static/js/runtime~main.25bc40f7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_phone-catalog/precache-manifest.9e30559dbd78e58839d05f395b4f4d51.js"
+  "/react_phone-catalog/precache-manifest.a2a08b769bcab4db3de48b5da818f47a.js"
 );
 
 self.addEventListener('message', (event) => {
